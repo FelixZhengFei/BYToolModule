83947f7d8d520a39aa2568b9beb1b34964802e1b
郑强飞 <704811790@qq.com>
2018-03-01 09:02:05 +0800

    UI

--- BYToolModule/BYToolModule/ViewController.swift
+++ BYToolModule/BYToolModule/ViewController.swift
@@ -9,24 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var pickerView1: FFOnePickerView?
+    private var educationModelArray=[PickViewCodeModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if ("16109847823".isPhoneNumber()) {
-            print("是")
-        } else {
-            print("否")
-
-        }
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonclicked(_ sender: Any) {
+        pickerView1 = FFOnePickerView.initialize()
+        pickerView1?.didInfoSelectedBlock = { (model) in
+        }
+        pickerView1?.dataArray=educationModelArray
+        pickerView1?.titleName="选择最高学历"
+        pickerView1?.show()
+    }
+    
 }
 
 
